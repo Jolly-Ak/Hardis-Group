40076b27784931ab908d2d03e7387c7f01aff877
tueurll <rayan08.nyabi@gmail.com>
2025-01-26 18:49:33 +0100

    comm

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 
 
-/* import footer like this fetch('../nav.html')
+/*
 .then(response => response.text())
 .then(data => document.getElementById('navbar').innerHTML = data)
 .catch(error => console.error('Erreur lors du chargement de la navigation:', error));
